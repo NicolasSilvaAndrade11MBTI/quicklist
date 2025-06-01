b7a55015bea0fb095e00a94af4943997fdfb80c3
Nicolas Andrade <marangg474@gmail.com>
2025-06-01 19:35:19 -0300

    adicao da validacão da imput do item do item para que o usuário não adicione um item sem nome

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
+    if (itemName === ""){
+    alert("Não é possivel adicionar um item em branco!")
+    return
+    }
+
     const item = {
         name: itemName,
         checked: false
@@ -21,11 +26,11 @@ function showItemsList() {
         sectionList.innerHTML += `
                 <div class="item">
                     <div>
-                        <input type="checkbox" name="list" id="item-${index}" ${item.checked ? 'checked' : ''}
-                        <div class="custom-checkbox" "onclick="checkItem('${item.name}')">
+                        <input type="checkbox" name="list" id="item-${index}">
+                        <div class="custom-checkbox">
                             <img src="./assets/checked.svg" alt="checked">
                         </div>
-                        <label for="item-${index}"onclick="checkItem('${item.name}')">${item.name}</label>
+                        <label for="item-${index}">${item.name}</label>
                     </div>
 
                     <button onclick="removeItem('${item.name}')">
@@ -53,18 +58,3 @@ function showItemsList() {
 
         showItemsList()
     }
-
-function checkItem(itemName) {
-    const item = items.find((item) => item.name === itemName)
-
- //   if(item.checked) {
-      //  item.checked = false
- //   } else {
-   //     item.checked = true
-  //  }
-
-  //item.checked ? item.checked = false : item.checked = true
-
-  item.checked = !item.checked
-  showItemsList()
-}
